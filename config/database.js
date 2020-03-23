0d469c4c165b90dbd6a0e923aeb61ac25cb24de6
Thais Carvalho <tata1br06@Thaiss-MacBook-Air.local>
2020-03-23 00:09:32 -0400

    first commit

---
+++ config/database.js
@@ -0,0 +1,7 @@
+// config/database.js
+module.exports = {
+
+    'url' : 'mongodb+srv://tata1br06:a@cluster0-2vv2x.mongodb.net/test?retryWrites=true&w=majority
+', // looks like mongodb://<user>:<pass>@mongo.onmodulus.net:27017/Mikha4ot
+    'dbName': 'test'
+};
